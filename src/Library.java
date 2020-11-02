b0e154a1feb6c95eaa0f82da428b99fdb1e87216
Mohammad Saleh <mohammad.saleh2@cs.huji.ac.il>
2020-11-03 00:04:27 +0200

    finished

--- src/Library.java
+++ src/Library.java
@@ -8,49 +8,41 @@ public class Library {
     int curBooksNum = 0;
     int curPatronsNum = 0;
     Book[] booksArray;
-    int[] booksId;
-
+    Patron[] patronsArray;
 
     /**
      * a library constructor
-     * @param maxBookCapacity - The maximal number of books this library can hold.
-     * @param maxBorrowedBooks - The maximal number of books this library allows a single patron to borrow at the
-     * same time.
+     *
+     * @param maxBookCapacity   - The maximal number of books this library can hold.
+     * @param maxBorrowedBooks  - The maximal number of books this library allows a single patron to borrow at the
+     *                          same time.
      * @param maxPatronCapacity - The maximal number of registered patrons this library can handle.
      */
-    Library(int maxBookCapacity, int maxBorrowedBooks, int maxPatronCapacity)
-    {
+    Library(int maxBookCapacity, int maxBorrowedBooks, int maxPatronCapacity) {
         maxBooksNum = maxBookCapacity;
         maxBorrowedByPatron = maxBorrowedBooks;
         maxPatronNum = maxPatronCapacity;
         booksArray = new Book[maxBooksNum]; // null filled array of Books
-        booksId = new int[maxBooksNum];
+        patronsArray = new Patron[maxPatronNum]; // null filled array of Patrons
     }
 
     /**
      * Adds the given book to this library, if there is place available, and it isn't already in the library.
+     *
      * @param book - The book to add to this library.
      * @return a non-negative id number for the book if there was a spot and the book was successfully added,
      * or if the book was already in the library; a negative number otherwise.
      */
-    int addBookToLibrary(Book book)
-    {
-        if(curBooksNum < maxBooksNum)
-        {
-            if(checkIfBookInLib(book))
-            {
-                for(int i=0; i < maxBooksNum ; i++)
-                {
-                    if(booksArray[i]== book)
-                        return i;
+    int addBookToLibrary(Book book) {
+        if (curBooksNum < maxBooksNum) {
+            for (int i = 0; i < maxBooksNum; i++) {
+                if (booksArray[i] == book) {
+                    return i;
                 }
             }
-            for (int i=0; i < maxBooksNum ; i++)
-            {
-                if (booksArray[i] == null)
-                {
+            for (int i = 0; i < maxBooksNum; i++) {
+                if (booksArray[i] == null) {
                     booksArray[i] = book;
-                    booksId[i] = i;
                     curBooksNum++;
                     return i;
                 }
@@ -59,17 +51,110 @@ public class Library {
         return -1;
     }
 
+
     /**
-     * checks if the given book is in the library or not.
-     * @param book - the checked book
-     * @return - true if the book is in the library, false otherwise.
+     * Returns true if the given number is an id of some book in the library, false otherwise.
+     *
+     * @param bookId - The id to check.
+     * @return true if the given number is an id of some book in the library, false otherwise.
      */
-    boolean checkIfBookInLib(Book book)
-    {
-        for(int i=0; i<maxBooksNum ; i++)
-        {
-            if(booksArray[i]== book)
-            {
+    boolean isBookIdValid(int bookId) {
+        return bookId >= 0 && bookId < maxBooksNum;
+    }
+
+    /**
+     * Returns the non-negative id number of the given book if he is owned by this library, -1 otherwise.
+     *
+     * @param book - The book for which to find the id number.
+     * @return a non-negative id number of the given book if he is owned by this library, -1 otherwise.
+     */
+    int getBookId(Book book) {
+        for (int i = 0; i < maxBooksNum; i++) {
+            if (booksArray[i] == book) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Returns true if the book with the given id is available, false otherwise.
+     *
+     * @param bookId - The id number of the book to check.
+     * @return true if the book with the given id is available, false otherwise.
+     */
+    boolean isBookAvailable(int bookId) {
+        return isBookIdValid(bookId) && booksArray[bookId] != null && booksArray[bookId].getCurrentBorrowerId() == -1;
+    }
+
+    /**
+     * Registers the given Patron to this library, if there is a spot available.
+     *
+     * @param patron - The patron to register to this library.
+     * @return a non-negative id number for the patron if there was a spot and the patron was successfully
+     * registered or if the patron was already registered. a negative number otherwise.
+     */
+    int registerPatronToLibrary(Patron patron) {
+        if (curPatronsNum < maxPatronNum) {
+            for (int i = 0; i < maxPatronNum; i++) {
+                if (patronsArray[i] == patron) {
+                    return i;
+                }
+            }
+            for (int i = 0; i < maxPatronNum; i++) {
+                if (patronsArray[i] == null) {
+                    patronsArray[i] = patron;
+                    curPatronsNum++;
+                    return i;
+                }
+            }
+        }
+        return -2;
+    }
+
+    /**
+     * Returns true if the given number is an id of a patron in the library, false otherwise.
+     *
+     * @param patronId - The id to check.
+     * @return true if the given number is an id of a patron in the library, false otherwise.
+     */
+    boolean isPatronIdValid(int patronId) {
+        return patronId >= 0 && patronId < maxPatronNum;
+    }
+
+    /**
+     * Returns the non-negative id number of the given patron if he is registered to this library, -1 otherwise.
+     *
+     * @param patron - The patron for which to find the id number.
+     * @return a non-negative id number of the given patron if he is registered to this library, -1 otherwise.
+     */
+    int getPatronId(Patron patron) {
+        for (int i = 0; i < maxPatronNum; i++) {
+            if (patronsArray[i] == patron) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Marks the book with the given id number as borrowed by the patron with the given patron id, if this
+     * book is available, the given patron isn't already borrowing the maximal number of books allowed, and if
+     * the patron will enjoy this book.
+     *
+     * @param bookId   - The id number of the book to borrow.
+     * @param patronId - The id number of the patron that will borrow the book.
+     * @return true if the book was borrowed successfully, false otherwise.
+     */
+    boolean borrowBook(int bookId, int patronId) {
+
+        if (isBookIdValid(bookId) && isPatronIdValid(patronId)) {
+            Patron thePatron = patronsArray[patronId];
+            Book theBook = booksArray[bookId];
+            if (isBookAvailable(bookId) && (thePatron.booksBorrowedByPatron < maxBorrowedByPatron)
+                    && thePatron.willEnjoyBook(theBook)) {
+                thePatron.booksBorrowedByPatron++;
+                theBook.setBorrowerId(patronId);
                 return true;
             }
         }
@@ -77,23 +162,40 @@ public class Library {
     }
 
     /**
-     * Returns true if the given number is an id of some book in the library, false otherwise.
-     * @param bookId - The id to check.
-     * @return true if the given number is an id of some book in the library, false otherwise.
+     * Return the given book.
+     *
+     * @param bookId - The id number of the book to return.
      */
-    boolean isBookIdValid(int bookId)
-    {
-        return bookId >= 0 && bookId < maxBooksNum;
+    void returnBook(int bookId) {
+        if (isBookIdValid(bookId) && booksArray[bookId].getCurrentBorrowerId() != -1) {
+            int curBorrowerID = booksArray[bookId].getCurrentBorrowerId();
+            patronsArray[curBorrowerID].booksBorrowedByPatron--;
+            booksArray[bookId].returnBook();
+        }
     }
 
     /**
-     * Returns the non-negative id number of the given book if he is owned by this library, -1 otherwise.
-     * @param book - The book for which to find the id number.
-     * @return a non-negative id number of the given book if he is owned by this library, -1 otherwise.
+     * Suggest the patron with the given id the book he will enjoy the most, out of all available books he will enjoy,
+     * if any such exist.
+     *
+     * @param patronId - The id number of the patron to suggest the book to.
+     * @return The available book the patron with the given ID will enjoy the most. Null if no book is available.
      */
-    int getBookId(Book book)
-    {
-        
+    Book suggestBookToPatron(int patronId) {
+        if (isPatronIdValid(patronId)) {
+            int mostEnjoyed = patronsArray[patronId].enjoymentThreshold;
+            Book mostEnjoyedBook = null;
+            for (int i = 0; i < maxBooksNum; i++) {
+                if (isBookAvailable(i) && patronsArray[patronId].willEnjoyBook(booksArray[i])) {
+                    if (patronsArray[patronId].getBookScore(booksArray[i]) > mostEnjoyed) {
+                        mostEnjoyed = patronsArray[patronId].getBookScore(booksArray[i]);
+                        mostEnjoyedBook = booksArray[i];
+                    }
+                }
+            }
+            return mostEnjoyedBook;
+        }
+        return null;
     }
 
 
--- src/Patron.java
+++ src/Patron.java
@@ -1,7 +1,9 @@
 import java.util.regex.Pattern;
 
 public class Patron {
-    /** data members of the class. */
+    /**
+     * data members of the class.
+     */
 
     final String firstName;
     final String lastName;
@@ -9,11 +11,13 @@ public class Patron {
     int dramaticVal;
     int educationalVal;
     int enjoymentThreshold;
+    int booksBorrowedByPatron = 0;
 
-    /** class constructor */
+    /**
+     * class constructor
+     */
     Patron(String patronFirstName, String patronLastName, int comicTendency,
-           int dramaticTendency, int educationalTendency, int patronEnjoymentThreshold)
-    {
+           int dramaticTendency, int educationalTendency, int patronEnjoymentThreshold) {
         firstName = patronFirstName;
         lastName = patronLastName;
         comicVal = comicTendency;
@@ -26,31 +30,31 @@ public class Patron {
      * Returns a string representation of the patron, which is a sequence of its first and last name,
      * separated by a single white space. For example, if the patron's first name is "Ricky" and his last name
      * is "Bobby", this method will return the String "Ricky Bobby".
+     *
      * @return the String representation of this patron.
      */
-    String stringRepresentation()
-    {
+    String stringRepresentation() {
         return firstName + " " + lastName;
     }
 
     /**
      * Returns the literary value this patron assigns to the given book.
      * Parameters:
+     *
      * @param book - The book to asses.
      * @return - the literary value this patron assigns to the given book.
      */
-    int getBookScore(Book book)
-    {
+    int getBookScore(Book book) {
         return comicVal * book.comicValue + dramaticVal * book.dramaticValue + educationalVal * book.educationalValue;
     }
 
     /**
      * Returns true if this patron will enjoy the given book, false otherwise.
+     *
      * @param book - The book to asses.
      * @return true if this patron will enjoy the given book, false otherwise.
      */
-    boolean willEnjoyBook(Book book)
-    {
+    boolean willEnjoyBook(Book book) {
         return this.getBookScore(book) > enjoymentThreshold;
     }
 }
